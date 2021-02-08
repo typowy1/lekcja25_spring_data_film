51935f01cd5d1a7df855838a4d63e5e3575138bc
Rafal Lesnicki <lesnickirafal@gmail.com>
2021-02-08 15:32:23 +0100

    g

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -10,27 +10,27 @@ import java.util.Optional;
 public class DemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        //  SpringApplication.run(DemoApplication.class, args);
 
-        //    uruchamianie aplikacji konsolowej pierw, wyswietli dane w konsoli
-//        ConfigurableApplicationContext context = SpringApplication.run(DemoApplication.class, args);
-//        UserRepesitory userRepesitory = context.getBean(UserRepesitory.class);
-//
-//        User user = new User();
-//        user.setName("Daniel");
-//        userRepesitory.save(user);
-//        displayUser(userRepesitory);
-//    }
-//
-//    private static void displayUser(UserRepesitory userRepesitory) {
-//        long id = 4L;
-//        Optional<User> userOptional = userRepesitory.findById(id);
-//        if (userOptional.isPresent()) { //jeśli nie zwruci null to wtedy działamy na użytkowniku
-//            User user = userOptional.get();
-//            System.out.println("Nazwa: " + user.getName());
-//        } else {
-//            System.out.println("Użytkownik o id: " + id + " nie istnieje!");
-//        }
+        // uruchamianie aplikacji konsolowej pierw, wyswietli dane w konsoli
+        ConfigurableApplicationContext context = SpringApplication.run(DemoApplication.class, args);
+        UserRepesitory userRepesitory = context.getBean(UserRepesitory.class);
+
+        User user = new User();
+        user.setName("Daniel");
+        userRepesitory.save(user);
+        displayUser(userRepesitory);
+    }
+
+    private static void displayUser(UserRepesitory userRepesitory) {
+        long id = 4L;
+        Optional<User> userOptional = userRepesitory.findById(id);
+        if (userOptional.isPresent()) { //jeśli nie zwruci null to wtedy działamy na użytkowniku
+            User user = userOptional.get();
+            System.out.println("Nazwa: " + user.getName());
+        } else {
+            System.out.println("Użytkownik o id: " + id + " nie istnieje!");
+        }
     }
 
 }
